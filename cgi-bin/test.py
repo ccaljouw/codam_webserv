77a10f0367359fb77813e94bd3a96065260413ac
Bruno Franco <bfranco@student.codam.nl>
2023-11-07 18:08:17 +0100

    seems to work

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+import datetime
+
+x = datetime.datetime.now()
+date = x.strftime("%a, %d %b %Y %H:%M:%S GMT")
+body = "<html><body><h1>It works!</h1></body></html>"
+
 print("HTTP/1.1 200\r")
-print("Content-Length: 405\r")
+print(f"Content-Length: {len(body)}\r")
 print("Content-type: text/html; charset=utf-8\r")
-print("Date: Tue, 07 Nov 2023 15:30:57 GMT\r")
-print("Last-Modified: Tue, 07 Nov 2023 15:30:57 GMT\r")
-print("Last-Modified: Tue, 07 Nov 2023 15:30:57 GMT\r")
+print(f"Date: {date}\r")
+print(f"Last-Modified: {date}\r")
+print(f"Last-Modified: {date}\r")
 print("Server: CODAM_WEBSERV\r")
 print("\r")
-
-print("<html><body><h1>It works!</h1></body></html>")
-
+print(body)
