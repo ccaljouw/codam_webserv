64e685344044795a2e8345d026ce2fbf1536f2f6
Bruno Franco <bfranco@student.codam.nl>
2023-11-24 12:05:40 +0100

    added timeout script

--- cgi-bin/delete.sh
+++ cgi-bin/delete.sh
@@ -1,52 +1,78 @@
 #!/usr/bin/env bash
 
-if [ -z "$QUERY_STRING"  && -z "$1"]
+status=""
+message=""
+filename=""
+
+# checks if the query string is empty and if no argument was given
+if [[ -z "$QUERY_STRING"  && -z "$1" ]]
 then
 	message="No file specified"
 	status="400"
 else
-	if [ -z $1 ]
+	# checks if an argument was given
+	if [ -z "$1" ]
 	then
-		FILENAME=$(echo "$QUERY_STRING" | sed -n 's/.*=\([^&]*\).*/\1/p' | sed 's/%20/ /g')
+		# gets the filename from the query string
+		filename=`echo "$QUERY_STRING" | awk -F'=' '{print $2}' | cut -d'&' -f1 | sed 's/%20/ /g'`
 	else
-		FILENAME="$1"
+		# gets the filename from the argument depending on the format
+		if [ -z `echo "$1" | grep "="` ]
+		then
+			filename="$1"
+		else
+			filename=`echo "$1" | awk -F'=' '{print $2}' | sed 's/%20/ /g'`
+		fi
 	fi
 fi
 
-if [ ! -z $status ]
+# checks if status was previously set
+if [ -z "$status" ]
 then
-	rm $FILENAME 2> /dev/null > /dev/null 
-
-	status=`echo $?`
-
-	if [ $status -eq 0 ]
+	# checks if the file exists
+	if [ -f "./uploads/$filename" ]
 	then
-		message="File deleted successfully"
-		status="200"
+		rm -rf "./uploads/$filename" 2> /dev/null > /dev/null 
+
+		# checks if the file was deleted successfully
+		if [ $? -eq 0 ]
+		then
+			message="File deleted successfully"
+			status="200"
+		else
+			message="File could not be deleted"
+			status="500"
+		fi
 	else
-		message="File could not be deleted"
-		status="500"
+		message="File does not exist"
+		status="404"
 	fi
 fi
 
+# sets the date
 date=`date -u +"%a, %d %b %Y %H:%M:%S GMT"`
 
 body="<html>
+	<head>
+		<link rel=\"icon\" href=\"data:,\">
+		<title>Delete File</title>
+	</head>
 	<body>
 		<h1>$message</h1>
 	</body>
 </html>\r\n"
 
+# gets the length of the body
 content_length=${#body}
 
-header="HTTP/1.1 $status\r\n\
-Content-Length: $content_length\r\n\
-Content-type: text/html; charset=utf-8\r\n\
-Date: $date\r\n\
-Last-Modified: $date\r\n\
-Connection: close\r\n\
+header="HTTP/1.1 $status\r
+Content-Length: $content_length\r
+Content-type: text/html; charset=utf-8\r\
+Date: $date\r
+Last-Modified: $date\r
+Connection: close\r
 Server: CODAM_WEBSERV\r\n\r"
-# Server: "$HOST"\r\n\r"
+# Server: $HOST\r\n\r"
 
 echo -e "$header"
 echo -e "$body"
